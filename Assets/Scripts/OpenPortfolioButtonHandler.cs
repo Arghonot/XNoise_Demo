939d97012052151f4747c7be31047b48a4ef6afd
Argonaut <lo.rivemale@gmail.com>
2026-04-26 17:39:32 +0700

    Reworked a couple of files to actually release a render texture after use.

--- Assets/Scripts/OpenPortfolioButtonHandler.cs
+++ Assets/Scripts/OpenPortfolioButtonHandler.cs
@@ -4,7 +4,7 @@ namespace XNoise_DemoWebglPlayer
     public class OpenPortfolioButtonHandler : MonoBehaviour
     {
         void Start() => UIManager.openPortfolioButtonClicked += OnOpenPortfolioButtonClicked;
-        private void OnDestroy() => UIManager.openPortfolioButtonClicked += OnOpenPortfolioButtonClicked;
+        private void OnDestroy() => UIManager.openPortfolioButtonClicked -= OnOpenPortfolioButtonClicked;
         private void OnOpenPortfolioButtonClicked() => Application.OpenURL("https://loick.rivemale.space/");
     }
 }
